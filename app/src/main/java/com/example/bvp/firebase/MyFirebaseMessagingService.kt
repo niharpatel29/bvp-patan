a6e1493a777a808e069bf754177ade2ae63ca8ed
Nihar Patel <niharpatel29@gmail.com>
2020-09-07 22:16:31 +0530

    Push notifications

--- app/src/main/java/com/example/bvp/firebase/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/bvp/firebase/MyFirebaseMessagingService.kt
@@ -10,7 +10,7 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import com.example.bvp.Login
+import com.example.bvp.Announcement
 import com.example.bvp.R
 import com.example.bvp.operations.Operations
 import com.example.bvp.prefs.SharedPref
@@ -25,6 +25,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         const val TAG = "fcmTAG"
     }
 
+    private lateinit var notificationManager: NotificationManager
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d(TAG, "token: $token")
@@ -37,35 +39,36 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         try {
             Log.d(TAG, "From: ${remoteMessage.from}")
-            Log.d(TAG, "To: ${remoteMessage.to}")
 
             val json = JSONObject(remoteMessage.data.toString())
+            Log.d(TAG, "JSON data: $json")
             showNotification(json)
         } catch (e: Exception) {
             Log.e(TAG, "Exception: ${e.message}")
         }
     }
 
-    data class JsonData(val title: String, val message: String)
-
     private fun showNotification(json: JSONObject) {
-        if (getDataFromJson(json) == null) {
+        if (getData(json) == null) {
             Log.e(TAG, "json data is null")
             return
         }
 
-        val title = getDataFromJson(json)?.title
-        val message = getDataFromJson(json)?.message
+        createChannel(json)
+        val title = getData(json)?.title
+        val message = getData(json)?.message
 
-        val intent = Intent(this, Login::class.java)
-        intent.flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val intent = Intent(this, Announcement::class.java).apply {
+            flags = (Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
         val pendingIntent =
             PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val builder =
-            NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+            NotificationCompat.Builder(this, getChannel(json)!!.id)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setContentIntent(pendingIntent)
@@ -75,15 +78,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
                 .setSmallIcon(R.drawable.ic_baseline_check_circle_filled)
                 .setColor(resources.getColor(R.color.colorPrimary))
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        notificationChannel(notificationManager)
         notificationManager.notify(999, builder.build())
     }
 
-    private fun getDataFromJson(json: JSONObject): JsonData? {
-        Log.d(TAG, "JSON data: $json")
+    data class Data(val title: String, val message: String)
+
+    private fun getData(json: JSONObject): Data? {
         return try {
             val data = json.getJSONObject("data")
 
@@ -91,7 +91,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             val message = data.getString("message")
 
             Log.d(TAG, "title: $title, msg: $message")
-            JsonData(title, message)
+            Data(title, message)
         } catch (e: JSONException) {
             Log.e(TAG, "Json Exception: ${e.message}")
             null
@@ -101,14 +101,50 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun notificationChannel(notificationManager: NotificationManager) {
+    private fun createChannel(json: JSONObject) {
+        val channelId = getChannel(json)?.id
+        val channelName = getChannel(json)?.name
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                getString(R.string.default_notification_channel_id),
-                getString(R.string.default_notification_channel_name),
-                NotificationManager.IMPORTANCE_HIGH
+            val importance = NotificationManager.IMPORTANCE_HIGH
+
+            val defaultChannel = NotificationChannel(
+                getString(R.string.channel_default_id),
+                getString(R.string.channel_default_name),
+                importance
             )
-            notificationManager.createNotificationChannel(notificationChannel)
+
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                importance
+            )
+
+            val channelList = ArrayList<NotificationChannel>()
+            channelList.add(defaultChannel)
+            channelList.add(channel)
+
+            notificationManager.createNotificationChannels(channelList)
+        }
+    }
+
+    data class Channel(val id: String, val name: String)
+
+    private fun getChannel(json: JSONObject): Channel? {
+        return try {
+            val channel = json.getJSONObject("channel")
+
+            val id = channel.getString("id")
+            val name = channel.getString("name")
+
+            Log.d(TAG, "id: $id, name: $name")
+            Channel(id, name)
+        } catch (e: JSONException) {
+            Log.e(TAG, "Json Exception: ${e.message}")
+            null
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception: ${e.message}")
+            null
         }
     }
 }
