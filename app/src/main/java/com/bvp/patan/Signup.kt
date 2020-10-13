55c8f94d7e9207c67b518f21ec0bbdd1fce4c107
Nihar Patel <niharpatel29@gmail.com>
2020-10-13 11:56:42 +0530

    Commit

--- app/src/main/java/com/bvp/patan/Signup.kt
+++ app/src/main/java/com/bvp/patan/Signup.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bvp.patan.api.APIInterface
 import com.bvp.patan.api.postClient
@@ -29,6 +30,16 @@ class Signup : AppCompatActivity() {
 
         toolbar()
         handleButtonClicks()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private fun toolbar() {
